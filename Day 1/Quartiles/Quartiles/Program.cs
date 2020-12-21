8bf849d8048881f3ae96a79d058a14032911650c
Alymzhan Kurbanov <alimk@ctechmanufacturing.com>
2020-12-21 17:09:11 -0600

    Day 1 - Quartiles
    Need to finish

---
+++ Day 1/Quartiles/Quartiles/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class Solution
+{
+    static void Main(String[] args)
+    {
+        var N = Convert.ToInt32(Console.ReadLine());
+
+        var arr = Console.ReadLine().Trim().Split().Select(x => Convert.ToInt32(x)).OrderBy(x => x).ToArray();
+
+        var median = N % 2 ==0 ? arr[N/2] : (arr[N-1]+arr[N])/2;
+
+        var L = 
+
+
+    }
+}
