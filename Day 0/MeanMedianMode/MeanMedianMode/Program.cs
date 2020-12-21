e6c5fedf8116d0d26b6d43f0909c12e226c1fcc5
Alymzhan Kurbanov <alymzhank@gmail.com>
2020-12-21 10:38:11 -0600

    Day 0
    2 Task of Day 0

---
+++ Day 0/MeanMedianMode/MeanMedianMode/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+class Solution
+{
+    static void Main(String[] args)
+    {
+        var N = Convert.ToInt32(Console.ReadLine());
+
+        var sum = 0.0d;
+        
+        var arr = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp));
+
+        Dictionary<int, int> dic = new Dictionary<int, int>();
+
+        for (int i = 0; i < N; i++)
+        {
+            sum += arr[i];
+            if (dic.ContainsKey(arr[i]))
+                dic[arr[i]] += 1;
+            else
+                dic.Add(arr[i], 1);
+        }
+
+        var median = 0.0;
+        var maxValue = dic.Max(x => x.Value);
+        var sortList = arr.OrderBy(x => x).ToList();
+        if (sortList.Count() % 2 == 0)
+        {
+            var z = sortList.Count() / 2;
+            median = (sortList[z - 1] + sortList[z]) / 2.0;
+        }
+        else
+        {
+            var z = sortList.Count() / 2;
+            median = sortList[z];
+        }
+
+        var sortList2 = dic.OrderBy(x => x.Key).ToList();
+
+        var mode = sortList2.Where(x => x.Value == maxValue).ToList().Min(x => x.Key);
+
+        var mainValue = sum / N;
+
+        Console.WriteLine(Math.Round(mainValue, 1));
+
+        Console.WriteLine(Math.Round(median, 1));
+
+        Console.WriteLine(mode);
+    }
+
+
+}
---
+++ Day 0/WeightedMean/WeightedMean/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class Solution
+{
+    static void Main(String[] args)
+    {
+        int N = Convert.ToInt32(Console.ReadLine());
+        var x = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp));
+        var w = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp));
+
+        double sumW = w.Sum(a => a);
+        double sumXW = 0.0d;
+
+        for (int i = 0; i < N; i++)
+        {
+            sumXW += x[i] * w[i];
+        }
+
+        double z = Math.Round(sumXW / sumW, 1);
+
+        Console.WriteLine("{0:0.0}", z);
+    }
+}
