ec7d0288d851aac7057d1a93d6bebf33c61e67c3
Alymzhan Kurbanov <alimk@ctechmanufacturing.com>
2020-12-22 09:19:08 -0600

    Finish

--- Day 1/Quartiles/Quartiles/Program.cs
+++ Day 1/Quartiles/Quartiles/Program.cs
@@ -6,14 +6,20 @@ class Solution
 {
     static void Main(String[] args)
     {
-        var N = Convert.ToInt32(Console.ReadLine());
+        var N = 10;// Convert.ToInt32(Console.ReadLine());
+        var s = "3 7 8 5 12 14 21 15 18 14";
+        var arr = s/*Console.ReadLine()*/.Trim().Split().Select(x => Convert.ToInt32(x)).OrderBy(x => x).ToArray();
 
-        var arr = Console.ReadLine().Trim().Split().Select(x => Convert.ToInt32(x)).OrderBy(x => x).ToArray();
+        var median = N % 2 == 0 ? (arr[N / 2 - 1] + arr[N / 2]) / 2 : arr[N / 2];
 
-        var median = N % 2 ==0 ? arr[N/2] : (arr[N-1]+arr[N])/2;
+        int L, U = 0;
 
-        var L = 
+        var Z = N / 4;
+        L = N / 2 % 2 == 0 ? (arr[Z - 1] + arr[Z]) / 2 : arr[Z];
+        U = N / 2 % 2 == 0 ? (arr[(N - Z)-1] + arr[(N - Z)]) / 2 : arr[N - Z - 1];
 
-
+        Console.WriteLine(L);
+        Console.WriteLine(median);
+        Console.WriteLine(U);
     }
 }
