79fe27da08504e0634d74cc520e84f42bc15cdd1
Alymzhan <alymzhank@gmail.com>
2020-12-22 21:32:45 -0600

    Completed

--- Day 1/InterquartileRange/InterquartileRange/Program.cs
+++ Day 1/InterquartileRange/InterquartileRange/Program.cs
@@ -6,22 +6,23 @@ class Solution
 {
     static void Main(String[] args)
     {
-        var s1 = "6 12 8 10 20 16";
-        var s2 = "5 4 3 2 1 5";
-
-        var N = 6; // Convert.ToInt32(Console.ReadLine());
-        var arr = s1/*Console.ReadLine()*/.Trim().Split().Select(x => Convert.ToInt32(x)).ToArray();
-        var f = s2/*Console.ReadLine()*/.Trim().Split().Select(x => Convert.ToInt32(x)).ToArray();
-        var w = f.Sum(x => x) * N;
+        var N = Convert.ToInt32(Console.ReadLine());
+        var arr = Console.ReadLine().Trim().Split().Select(x => Convert.ToInt32(x)).ToArray();
+        var f = Console.ReadLine().Trim().Split().Select(x => Convert.ToInt32(x)).ToArray();
+        var w = f.Sum(x => x);
         int[] arrF = new int[w];
+        int q = 0;
 
-        for (int i=0; i<N; i++)
+        for (int i = 0; i < N; i++)
         {
-            for (int j=0; j<f[i]; j++)
+            for (int j = 0; j < f[i]; j++)
             {
-                arrF[i + j] = arr[i];
+                arrF[q] = arr[i];
+                q++;
             }
         }
+
+        arrF = arrF.Select(x => x).OrderBy(x => x).ToArray();
 
         double L, U = 0.0d;
 
@@ -29,6 +30,7 @@ class Solution
         L = w / 2 % 2 == 0 ? (arrF[z - 1] + arrF[z]) / 2 : arrF[z];
         U = w / 2 % 2 == 0 ? (arrF[(w - z) - 1] + arrF[(w - z)]) / 2 : arrF[w - z - 1];
 
-        Console.WriteLine(Math.Round(U-L,1));
+        Console.WriteLine("{0:0.0}", Math.Round(U - L, 1));
+
     }
 }
